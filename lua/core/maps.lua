93f6ca8405ae48d28256f17bcaf1c87c0091d76f
jhenriquem <machadojh11@gmail.com>
2024-01-16 23:45:15 -0300

    bug fixes and new mappings

--- lua/core/maps.lua
+++ lua/core/maps.lua
@@ -1,13 +1,13 @@
 local keymap = vim.keymap
 
-keymap.set('n', 'x', '"_x')
+keymap.set("n", "x", '"_x')
 
 keymap.set("n", "n", ":enew<cr>")
-keymap.set('n', 'w', ':w ')
+keymap.set("n", "w", ":w ")
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
 
 keymap.set("n", "<space>a", ":bp<cr>")
 keymap.set("n", "<space>x", ":bd<cr>")
@@ -28,7 +28,6 @@ keymap.set("i", "<C-l>", "<Right>")
 keymap.set("i", "<C-h>", "<Left>")
 keymap.set("i", "<C-k>", "<Down>")
 
-
 keymap.set("i", "<Up>", "")
 keymap.set("i", "<Right>", "")
 keymap.set("i", "<Left>", "")
@@ -38,36 +37,35 @@ keymap.set("i", "<Down>", "")
 keymap.set("n", "<space>w", ":w!<cr>")
 keymap.set("n", "q", ":q<cr>")
 
-keymap.set('n', '<space>n',':NvimTreeToggle<cr>')
+keymap.set("n", "<space>n", ":NvimTreeToggle<cr>")
 ------
 keymap.set("n", ";", ":")
 
-keymap.set("i", "´´", "''<Left>")
-keymap.set('i',"´", "'")
-keymap.set("i", "ll", "<ESC>")
-keymap.set("i", "kk", "<ESC>v")
+keymap.set("i", "~~", "''<Left>")
+keymap.set("i", "~", "'")
+keymap.set("i", "<A-[>", "()<Left>")
+keymap.set("i", "çç", "<ESC>")
+keymap.set("i", "çl", "<ESC>v")
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
-
+keymap.set("n", "dw", 'vb"_d')
 -- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G')
-
+keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- New tab
-keymap.set('n', 'te', ':tabedit')
+keymap.set("n", "te", ":tabedit")
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+keymap.set("n", "ss", ":split<Return><C-w>w")
+keymap.set("n", "sv", ":vsplit<Return><C-w>w")
 -- Move window
-keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', '<space>h', '<C-w>h')
-keymap.set('', '<space>k', '<C-w>k')
-keymap.set('', '<space>j', '<C-w>j')
-keymap.set('', '<space>l', '<C-w>l')
+keymap.set("n", "<Space>", "<C-w>w")
+keymap.set("", "<space>h", "<C-w>h")
+keymap.set("", "<space>k", "<C-w>k")
+keymap.set("", "<space>j", "<C-w>j")
+keymap.set("", "<space>l", "<C-w>l")
 
 -- Resize window
-keymap.set('n', '<C-right>', '<C-w><')
-keymap.set('n', '<C-left>', '<C-w>>')
-keymap.set('n', '<C-up>', '<C-w>+')
-keymap.set('n', '<C-down>', '<C-w>-')
+keymap.set("n", "<C-right>", "<C-w><")
+keymap.set("n", "<C-left>", "<C-w>>")
+keymap.set("n", "<C-up>", "<C-w>+")
+keymap.set("n", "<C-down>", "<C-w>-")
