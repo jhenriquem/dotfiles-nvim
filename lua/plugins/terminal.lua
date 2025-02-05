7782e4e21594c2671899cc4632a3c8668bf23f4c
jhenriquem <machadojh11@gmail.com>
2025-02-05 11:38:36 -0300

    add toggleterm.nvim plugin

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,30 @@
+return {
+	"akinsho/toggleterm.nvim",
+	lazy = false,
+	version = "*",
+	config = function()
+		local toggleterm = require("toggleterm")
+
+		toggleterm.setup({
+			size = 8,
+			open_mapping = [[<c-\>]],
+			hide_numbers = true,
+			shell = "pwsh.exe",
+			shade_filetypes = {},
+			shade_terminals = true,
+			shading_factor = 2,
+			start_in_insert = true,
+			insert_mappings = true,
+			persist_size = true,
+			direction = "float",
+			close_on_exit = true,
+		})
+
+		function _G.set_terminal_keymaps()
+			local opts = { noremap = true }
+			vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+		end
+
+		vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+	end,
+}
