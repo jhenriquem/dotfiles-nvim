eee511f1bc80ec46687759f4b6d5fe98b2678448
jhenriquem <machadojh11@gmail.com>
2025-02-06 23:03:40 -0300

    new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,26 @@
 return {
 	{
+		"gbprod/nord.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nord").setup({
+				transparent = true,
+				styles = {
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+			})
+			-- vim.cmd.colorscheme("nord")
+		end,
+	},
+
+	{
 		--
 		"folke/tokyonight.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			vim.cmd.colorscheme("tokyonight-night")
-
 			vim.cmd.hi("Comment gui=none")
 		end,
 		config = function()
