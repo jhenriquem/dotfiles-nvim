84b6f4102dccd5b7c430176019c83eb428ee9103
jhenriquem <machadojh11@gmail.com>
2025-02-06 23:15:26 -0300

    add noice.nvim plugin

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,52 @@
+return {
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			require("notify").setup({
+				merge_duplicates = false,
+				timeout = 2000,
+				background_colour = "#000000",
+				render = "wrapped-compact",
+			})
+			-- vim.notify = require("notify")
+		end,
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+		config = function()
+			require("noice").setup({
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+					signature = { enabled = false },
+					hover = {
+						enabled = false,
+					},
+				},
+				routes = {
+					{
+						filter = {
+							event = "msg_show",
+							any = {
+								{ find = "%d+L, %d+B" },
+								{ find = "; after #%d+" },
+								{ find = "; before #%d+" },
+							},
+						},
+						view = "mini",
+					},
+				},
+				presets = {
+					bottom_search = true,
+					command_palette = true,
+					long_message_to_split = true,
+				},
+			})
+		end,
+	},
+}
