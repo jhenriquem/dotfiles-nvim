da6001e14ca03f6703ef90a4d1a43d74886fe5ac
jhenriquem <machadojh11@gmail.com>
2025-02-06 23:06:16 -0300

    add new lsp servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,25 +20,17 @@ return {
 					end
 
 					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
 					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
 					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
 					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
 					map(
 						"<leader>ws",
 						require("telescope.builtin").lsp_dynamic_workspace_symbols,
 						"[W]orkspace [S]ymbols"
 					)
-
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
@@ -121,6 +113,9 @@ return {
 			require("mason-tool-installer").setup({
 				ensure_installed = {
 					"stylua",
+					"prettier",
+					"goimports",
+					"gofumpt",
 				},
 				automatic_installation = true,
 			})
